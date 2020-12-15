c8038b4c2fc99c4b12c83395fbc91b691b2ef432
RodrigoAlanisWeb <Arkpex2007ps4@gmail.com>
2020-12-14 21:49:23 -0600

    Migraciones Terminadas

--- Take-A-Photo/database/migrations/2020_12_15_032617_create_users_table.php
+++ Take-A-Photo/database/migrations/2020_12_15_032617_create_users_table.php
@@ -16,10 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('nick_name');
+            $table->string('email',245);
+            $table->unique('email');
+            $table->string('password',245);
+            $table->string('role');
+            $table->string('status');
             $table->timestamps();
         });
     }
---
+++ Take-A-Photo/database/migrations/2020_12_15_033419_create_images_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->unsignedBigInteger('imageable_id');
+            $table->string('imageable_type',245);
+            $table->string('path',245);
+
+            $table->primary(['imageable_id','imageable_type']);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
